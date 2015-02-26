50cac4480bed285c338cebcd36bdcd2706fb14ec
phoebelb <phoebelb@corn39.stanford.edu>
2015-02-25 21:24:47 -0800

    I meowved it.

---
+++ homework/helper.sh
@@ -0,0 +1,7 @@
+cd baby-name-gender-detector
+mkdir -p ./data-hold
+cd data-hold
+curl -o names.zip http://stash.compciv.org/ssa_baby_names/names.zip
+unzip -p names.zip yob1973.txt yob2013.txt | dos2unix>namesample.text
+cd ..
+
